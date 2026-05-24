a60765f301d9bb695688ab52284c7dda35faae2f
Edward Garcia <egarciag97@hotmail.com>
2026-05-24 12:05:00 -0500

    init

---
+++ src/test/java/com/childrencare/ChildrenCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.childrencare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
